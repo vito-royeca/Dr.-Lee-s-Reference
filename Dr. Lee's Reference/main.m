b2fc88e6d145a8e4020b256e6dc77954d461ef44
Jovito Royeca <jovito.royeca@cloudsherpas.com>
2013-11-26 03:11:10 +0800

    Initial Commit

---
+++ Dr. Lee's Reference/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dr. Lee's Reference
+//
+//  Created by Jovito Royeca on 11/26/13.
+//  Copyright (c) 2013 Jovito Royeca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
